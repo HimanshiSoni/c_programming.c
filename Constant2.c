d786e943658c5b5af59431364eb24509a26c1bbb
Himanshi Soni <78069161+HimanshiSoni@users.noreply.github.com>
2021-05-21 17:55:43 +0530

    Update Constant2.c

--- Constant2.c
+++ Constant2.c
@@ -2,7 +2,7 @@
 int main()
 {
    const float a=10;
-   const char b=11;
+   const char b='a';
    const double c=12;
    const int d=13;
    printf("\n float %f \n char %c \n double %lf \n int %d",a,b,c,d);
