5f2e8db63995fad3acae57aba431405b827f8dc2
Himanshi Soni <78069161+HimanshiSoni@users.noreply.github.com>
2021-05-21 17:53:20 +0530

    Create Constant2.c

---
+++ Constant2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+   const float a=10;
+   const char b=11;
+   const double c=12;
+   const int d=13;
+   printf("\n float %f \n char %c \n double %lf \n int %d",a,b,c,d);
+   return 0;
+}
