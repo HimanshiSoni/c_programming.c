c238d179fa3e5a2f576ade3e20400d3c5b866f0d
Himanshi Soni <78069161+HimanshiSoni@users.noreply.github.com>
2021-05-22 20:34:45 +0530

    Create Gross salary.c

---
+++ Gross salary.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+   int a,b,s;
+   printf("enter salary as per hr and person's working hr:");
+   scanf("%d%d",&a,&b);
+   s=a*b;
+   printf("\ngross salary:%d",s);
+   return 0;
+}
