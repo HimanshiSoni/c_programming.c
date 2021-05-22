724eef996f01cc79d3d8a2d8b7c41409d2ae6a38
Himanshi Soni <78069161+HimanshiSoni@users.noreply.github.com>
2021-05-22 18:40:06 +0530

    Create Use of \n \r \t.c

---
+++ Use of \n \r \t.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+   printf("next line\n");  //Using \n
+   printf("move the cursor at the starting of line\r");  //Using \r
+   printf("space\t");  //Using \t
+   return 0;
+}
