103d21f077d5a64ffeed5d51b1d730b9e7bdcf83
Himanshi Soni <78069161+HimanshiSoni@users.noreply.github.com>
2021-05-22 18:18:00 +0530

    Create Average.c

---
+++ Average.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+   int a,b,c,d,e,f;
+   printf("enter five values:");
+   scanf("%d %d %d %d %d",&a,&b,&c,&d,&e);
+   f=(a+b+c+d+e)/5;
+   printf("addition:%d",f);
+   return 0;
+}
